7a61acce678bbff12b242a6f6a2531f70edf031e
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-25 22:39:10 +0300

    created MeVC and configured ui interface

---
+++ FriendlyChat/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  FriendlyChat
+//
+//  Created by Kaiserdem on 25.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+  
+  @IBOutlet weak var emailLabel: UILabel!
+  
+  @IBOutlet weak var profileImage: UIImageView!
+  
+  @IBOutlet weak var tableView: UITableView!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  @IBAction func singInOutWasPressed(_ sender: Any) {
+  }
+}
