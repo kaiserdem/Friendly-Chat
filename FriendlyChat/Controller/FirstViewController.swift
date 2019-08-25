1803dcd77f1f2f7f3750219aef2061db27e9d41d
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-25 18:04:22 +0300

    created project, main pattern folder, file manager, and firebase is connected

---
+++ FriendlyChat/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FriendlyChat
+//
+//  Created by Kaiserdem on 25.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ FriendlyChat/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FriendlyChat
+//
+//  Created by Kaiserdem on 25.08.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+
+}
+
