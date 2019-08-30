646ab429850b88a2b0d894d711facbfbaf6aa07a
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-08-30 20:54:43 +0300

    downloading all message for a group and animating upon new message

--- FriendlyChat/Controller/GroupFeedVC.swift
+++ FriendlyChat/Controller/GroupFeedVC.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupFeedVC: UIViewController {
-
+  
   @IBOutlet weak var groupTitleLabel: UILabel!
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var membersLabel: UILabel!
@@ -18,6 +19,7 @@ class GroupFeedVC: UIViewController {
   @IBOutlet weak var sendBtnView: UIView!
   
   var group: Group?
+  var groupMessages = [Message]()
   
   func initGroupData(forGroup group: Group) {
     self.group = group
@@ -25,10 +27,12 @@ class GroupFeedVC: UIViewController {
   }
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
     sendBtnView.bindToKeyboard()
-      
-    }
+    tableView.delegate = self
+    tableView.dataSource = self
+    
+  }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
@@ -36,15 +40,53 @@ class GroupFeedVC: UIViewController {
     DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
       self.membersLabel.text = returnedEmails.joined(separator: ", ")
     }
+    DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+      DataService.instance.getAllMessagesFor(desiderGroup: self.group!, handler: { (returnedGroupMessages) in
+        self.groupMessages = returnedGroupMessages
+        self.tableView.reloadData()
+        
+        if self.groupMessages.count > 0 {
+          self.tableView.scrollToRow(at: IndexPath(row: self.groupMessages.count - 1, section: 0), at: .none, animated: true)
+        }
+      })
+    }
   }
-    
+  
   @IBAction func backBtnWasPressed(_ sender: Any) {
     dismiss(animated: true, completion: nil)
   }
   
   @IBAction func sendBtnWaaPressed(_ sender: Any) {
-    
+    if messageTextField.text != "" {
+      messageTextField.isEnabled = false
+      sendButton.isEnabled = false
+      DataService.instance.uploadPost(withMessage: messageTextField.text!, forUID: (Auth.auth().currentUser?.uid)!, withGroupKey: group?.key) { (complete) in
+        if complete {
+          self.messageTextField.text = ""
+          self.messageTextField.isEnabled = true
+          self.sendButton.isEnabled = true
+        }
+      }
+    }
+  }
+}
+
+extension GroupFeedVC: UITableViewDelegate, UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return groupMessages.count
   }
   
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "groupFeedCell", for: indexPath) as? GroupFeedCell else { return UITableViewCell() }
+    
+    let message = groupMessages[indexPath.row]
+    DataService.instance.getUserName(forUID: message.senderId) { (email) in
+      cell.configureCell(profileImage: UIImage(named: "defaultProfileImage")!, email: email, content: message.content)
+    }
+    return cell
+}
 
+func numberOfSections(in tableView: UITableView) -> Int {
+  return 1
+}
 }
--- FriendlyChat/Services/DataService.swift
+++ FriendlyChat/Services/DataService.swift
@@ -49,7 +49,8 @@ class DataService {
   }
   func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupKey: String?, sendComlete: @escaping (_ status: Bool) -> ()) {
     if groupKey != nil {
-      // send to groups ref
+      REF_GROUPS.child(groupKey!).child("messages").childByAutoId().updateChildValues(["content": message, "senderId": uid])
+      sendComlete(true)
     } else {
       REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId": uid])
       sendComlete(true)
@@ -83,6 +84,22 @@ class DataService {
       handler(emailArray)
     }
   }
+  
+  func getAllMessagesFor(desiderGroup: Group, handler: @escaping (_ messagesArray: [Message]) ->
+    ()) {
+    var groupMessageArray = [Message]()
+    REF_GROUPS.child(desiderGroup.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+      guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else { return }
+      for groupMessage in groupMessageSnapshot {
+        let content = groupMessage.childSnapshot(forPath: "content").value as! String
+        let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+        let groupMessage = Message(content: content, senderId: senderId)
+        groupMessageArray.append(groupMessage)
+      }
+      handler(groupMessageArray)
+    }
+  }
+  
   func getIds(forUserNames userNames:[String], handler: @escaping (_ uidArray: [String]) -> ()) {
     REF_USERS.observeSingleEvent(of: .value) { (userSnapshot) in
       var idArray = [String]()
